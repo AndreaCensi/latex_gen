fecc7c1e006fd54f035ce97c9aeb6eeb1cbcada6
Andrea Censi <acensi@ethz.ch>
2021-03-01 20:53:33 +0100

    templates

---
+++ setup.py
@@ -0,0 +1,27 @@
+import json
+
+from setuptools import setup
+
+with open("setup.json") as f:
+    data = json.load(f)
+
+# setup package
+params = dict(
+    name=data["package_name"],
+    author=data["author"],
+    author_email=data["author_email"],
+    url=data["url"],
+    tests_require=data["tests_require"],
+    install_requires=data["install_requires"],
+    package_dir={"": data["srcdir"]},
+    packages=data["modules"],
+    long_description="",
+    version=data["version"],
+    entry_points=data["entry_points"],
+    zip_safe=data["zip_safe"],
+    include_package_data=data["include_package_data"],
+)
+
+setup(**params)
+
+# sigil db3b1eaa6e910979b80cf8826d500c83

10128fb7a759118dd1cb3877a2c7e5b5e041942e
Andrea Censi <acensi@ethz.ch>
2021-03-01 20:53:02 +0100

    fix

--- src/latex_gen/__init__.py
+++ src/latex_gen/__init__.py
@@ -1,3 +1,10 @@
+__version__ = "7.1.2103012049"
+__date__ = "2021-03-01T20:49:19.166261"
+from zuper_commons.logs import ZLogger
+
+logger = ZLogger(__name__)
+logger.hello_module(name=__name__, filename=__file__, version=__version__, date=__date__)
+
 from .structures import *
 from .compile_latex import *
 

825b4930c83c4da59087534a97cd43c1723c9978
Andrea Censi <acensi@ethz.ch>
2021-05-01 10:43:26 +0200

    Bump to version 7.1.2105010843

--- src/latex_gen/__init__.py
+++ src/latex_gen/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2105010823"
-__date__ = "2021-05-01T08:23:27.521886+00:00"
+__version__ = "7.1.2105010843"
+__date__ = "2021-05-01T08:43:26.283341+00:00"
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)

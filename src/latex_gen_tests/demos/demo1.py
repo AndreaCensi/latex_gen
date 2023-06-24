c59ef1c15b04f69f277a7204085e3626bbc607c1
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-06-24 13:59:30 +0200

    ciskip

--- src/latex_gen_tests/demos/demo1.py
+++ src/latex_gen_tests/demos/demo1.py
@@ -6,7 +6,6 @@ def demo1(doc):
     doc.text("This is an example with a figures with two subfigures.")
 
     with doc.figure(caption="") as fig:
-
         with fig.subfigure(caption="fig1") as sub:
             sub.text("ciao")
 
